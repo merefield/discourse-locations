f63a8f201ce17152da70a33584f9ebe25ac8b170
merefield <merefield@gmail.com>
2026-01-11 16:54:10 +0000

    FIX: make DMenu work on modals!

--- assets/javascripts/discourse/components/geo-d-multi-select.gjs
+++ assets/javascripts/discourse/components/geo-d-multi-select.gjs
@@ -13,11 +13,12 @@ import DMenu from "discourse/float-kit/components/d-menu";
 import concatClass from "discourse/helpers/concat-class";
 import icon from "discourse/helpers/d-icon";
 import element from "discourse/helpers/element";
+import uniqueId from "discourse/helpers/unique-id";
 import discourseDebounce from "discourse/lib/debounce";
 import { INPUT_DELAY } from "discourse/lib/environment";
 import { makeArray } from "discourse/lib/helpers";
 import scrollIntoView from "discourse/modifiers/scroll-into-view";
-import { eq } from "discourse/truth-helpers";
+import { and, eq, not } from "discourse/truth-helpers";
 import { i18n } from "discourse-i18n";
 
 class Skeleton extends Component {
@@ -44,11 +45,19 @@ export default class GeoDMultiSelect extends Component {
   @tracked value = null;
   @tracked error = null;
 
+  // Float-kit: override portal outlet (so modals can trap focus correctly)
+  @tracked portalOutletElement = null;
+  @tracked inModal = false;
+
   // when true, the next resolved search will auto-pick first result
   @tracked autoPickNextResult = false;
 
   compareKey = "id";
   _requestId = 0;
+
+  get identifier() {
+    return `geo-d-multi-select-${uniqueId()}`;
+  }
 
   get hasSelection() {
     return (this.args.selection?.length ?? 0) > 0;
@@ -68,6 +77,33 @@ export default class GeoDMultiSelect extends Component {
         !this.args.selection?.some((selected) => this.compare(item, selected))
     );
   }
+  @action
+  setPortalOutlet(triggerEl) {
+    const modalRoot =
+      triggerEl.closest(
+        ".d-modal__container, .d-modal, .modal, .bootbox.modal, .fk-modal"
+      ) || null;
+
+    this.inModal = !!modalRoot;
+
+    if (!modalRoot) {
+      this.portalOutletElement = null;
+      return;
+    }
+
+    let outlet =
+      modalRoot.querySelector("#location-modal-menu-portals") ||
+      modalRoot.querySelector(".location-modal-menu-portals");
+
+    if (!outlet) {
+      outlet = document.createElement("div");
+      outlet.id = "location-modal-menu-portals";
+      outlet.className = "location-modal-menu-portals";
+      modalRoot.appendChild(outlet);
+    }
+
+    this.portalOutletElement = outlet;
+  }
 
   #debouncedSearch() {
     discourseDebounce(
@@ -81,7 +117,6 @@ export default class GeoDMultiSelect extends Component {
 
   @action
   search(event) {
-    debugger;
     // normal typing path: DO NOT auto-pick
     this.autoPickNextResult = false;
 
@@ -103,7 +138,6 @@ export default class GeoDMultiSelect extends Component {
 
   @action
   handleKeydown(event) {
-    debugger;
     if (!this.isResolved) {
       return;
     }
@@ -186,19 +220,14 @@ export default class GeoDMultiSelect extends Component {
   compare(a, b) {
     if (this.args.compareFn) {
       return this.args.compareFn(a, b);
-    } else {
-      return a?.[this.compareKey] === b?.[this.compareKey];
     }
+    return a?.[this.compareKey] === b?.[this.compareKey];
   }
 
   getDisplayText(item) {
     return item?.name;
   }
 
-  // Choose the first "real" location result:
-  // - skip provider/footer item (your code adds { provider: ... })
-  // - skip anything falsy
-  // - skip anything already selected (availableOptions already filters selection, but keep safe)
   #firstSelectableResult() {
     const opts = this.availableOptions || [];
     return opts.find((r) => r && !r.provider);
@@ -217,7 +246,6 @@ export default class GeoDMultiSelect extends Component {
       return;
     }
 
-    // mimic a click selection
     const currentSelection = makeArray(this.args.selection);
     this.args.onChange?.(currentSelection.concat(first));
   }
@@ -239,16 +267,15 @@ export default class GeoDMultiSelect extends Component {
         this.value = val;
         this.isResolved = true;
 
-        // if bullseye triggered the search, auto-select the first result
         this.#autoPickIfNeeded();
       })
       .catch((e) => {
         if (requestId !== this._requestId) {
           return;
         }
+
         this.error = e;
         this.isRejected = true;
-        // no auto-pick on error
         this.autoPickNextResult = false;
       })
       .finally(() => {
@@ -265,7 +292,6 @@ export default class GeoDMultiSelect extends Component {
       return;
     }
 
-    // next search (coords) should auto-pick
     this.autoPickNextResult = true;
 
     navigator.geolocation.getCurrentPosition(
@@ -274,7 +300,7 @@ export default class GeoDMultiSelect extends Component {
         this.preselectedItem = null;
         this.searchTerm = term;
 
-        // run immediately (debounced is fine too, but immediate makes UX snappier)
+        // Run immediately so we can auto-pick right away
         this.#performSearch(this.args.loadFn, term);
       },
       () => {
@@ -285,20 +311,33 @@ export default class GeoDMultiSelect extends Component {
 
   <template>
     <DMenu
-      @identifier="d-multi-select"
+      @identifier={{this.identifier}}
+      @listeners={{false}}
+      @portalOutletElement={{this.portalOutletElement}}
+      @inline={{this.inModal}}
       @triggerComponent={{element "div"}}
-      @triggerClass={{concatClass (if this.hasSelection "--has-selection")}}
+      @triggerClass={{concatClass
+        "d-multi-select-trigger"
+        "geo-d-multi-select-trigger-root"
+        (if this.hasSelection "--has-selection")
+      }}
+      @contentClass={{concatClass
+        "d-multi-select-content"
+        "geo-d-multi-select-content-root"
+      }}
       @visibilityOptimizer={{@visibilityOptimizer}}
       @placement={{@placement}}
       @allowedPlacements={{@allowedPlacements}}
       @offset={{@offset}}
       @matchTriggerMinWidth={{@matchTriggerMinWidth}}
       @matchTriggerWidth={{@matchTriggerWidth}}
-      @renderInPlace={{true}}
       ...attributes
     >
-      <:trigger>
-        <div class="geo-d-multi-select-trigger">
+      <:trigger as |api|>
+        <div
+          class="geo-d-multi-select-trigger"
+          {{didInsert this.setPortalOutlet}}
+        >
           <div class="geo-d-multi-select-trigger__content">
             {{#if @selection}}
               <div class="d-multi-select-trigger__selection">
@@ -332,7 +371,8 @@ export default class GeoDMultiSelect extends Component {
             <DButton
               @icon="angle-down"
               class="d-multi-select-trigger__expand-btn btn-transparent"
-              @action={{@componentArgs.show}}
+              @disabled={{and this.inModal (not this.portalOutletElement)}}
+              @action={{api.show}}
             />
           </div>
         </div>
--- assets/javascripts/discourse/initializers/location-edits.js
+++ assets/javascripts/discourse/initializers/location-edits.js
@@ -1,8 +1,8 @@
+import { computed } from "@ember/object";
 import { scheduleOnce } from "@ember/runloop";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import Composer from "discourse/models/composer";
 import NavItem from "discourse/models/nav-item";
-import { action, computed } from "@ember/object";
 import {
   default as discourseComputed,
   observes,
@@ -19,189 +19,236 @@ export default {
         name: "nearby",
         href: "/nearby",
       });
-      api.modifyClass("controller:users", Superclass => class extends Superclass {
-        loadUsers(params) {
-          if (params !== undefined && params.period === "location") {
-            return;
-          }
-          this._super(params);
-        }
-      });
-      api.modifyClass("model:composer", Superclass => class extends Superclass {
-        @computed(
-          "subtype",
-          "categoryId",
-          "topicFirstPost",
-          "forceLocationControls"
-        )
-        get showLocationControls() {
-          const subtype = this.get("subtype");
-          const categoryId = this.get("categoryId");
-          const topicFirstPost = this.get("topicFirstPost");
-          const force = this.get("forceLocationControls");
-
-          //(subtype, categoryId, topicFirstPost, force)
-          if (!topicFirstPost) {
-            return false;
-          }
-          if (force) {
-            return true;
-          }
-          if (categoryId) {
-            const category = this.site.categories.findBy("id", categoryId);
-            if (category && category.custom_fields?.location_enabled) {
-              return true;
-            }
-          }
-          return false;
-        }
-
-        clearState() {
-          this._super(...arguments);
-          this.set("location", null);
-        }
-
-        @observes("draftKey")
-        _setupDefaultLocation() {
-          if (this.draftKey.startsWith(NEW_TOPIC_KEY)) {
-            const topicDefaultLocation =
-              this.siteSettings.location_topic_default;
-            // NB: we can't use the siteSettings, nor currentUser values set in the initialiser here
-            // because in QUnit they will not be defined as the initialiser only runs once
-            // so this will break all tests, even if in runtime it may work.
-            // so solution is to use the values provided by the Composer model under 'this'.
-            if (
-              topicDefaultLocation === "user" &&
-              this.user.custom_fields.geo_location &&
-              ((typeof this.user.custom_fields.geo_location === "string" &&
-                this.user.custom_fields.geo_location.replaceAll(" ", "") !==
-                  "{}") ||
-                (typeof this.user.custom_fields.geo_location === "object" &&
-                  Object.keys(this.user.custom_fields.geo_location).length !==
-                    0))
-            ) {
-              this.set("location", {
-                geo_location: this.user.custom_fields.geo_location,
-              });
-            }
-          }
-        }
-      });
-
-      api.modifyClass("component:composer-body", Superclass => class extends Superclass {
-        @observes("composer.location")
-        resizeWhenLocationAdded() {
-          this._triggerComposerResized();
-        }
-
-        @observes("composer.showLocationControls", "composer.composeState")
-        applyLocationInlineClass() {
-          const applyClasses = () => {
-            const showLocationControls = this.get(
-              "composer.showLocationControls"
-            );
-            const containerElement = document.querySelector(
-              ".composer-fields .title-and-category"
-            );
-
-            if (containerElement) {
-              // Toggle the "show-location-controls" class based on `showLocationControls`
-              if (showLocationControls) {
-                containerElement.classList.add("show-location-controls");
-              } else {
-                containerElement.classList.remove("show-location-controls");
+      api.modifyClass(
+        "controller:users",
+        (Superclass) =>
+          class extends Superclass {
+            loadUsers(params) {
+              if (params !== undefined && params.period === "location") {
+                return;
               }
+              super.loadUsers(params);
+            }
+          }
+      );
+      api.modifyClass(
+        "model:composer",
+        (Superclass) =>
+          class extends Superclass {
+            @computed("categoryId", "topicFirstPost", "forceLocationControls")
+            get showLocationControls() {
+              const categoryId = this.get("categoryId");
+              const topicFirstPost = this.get("topicFirstPost");
+              const force = this.get("forceLocationControls");
 
-              if (showLocationControls) {
-                const anchorElement = this.site.mobileView
-                  ? containerElement.querySelector(".title-input")
-                  : containerElement;
-
-                // Move ".composer-controls-location" element to `anchorElement`
-                const locationControl = document.querySelector(
-                  ".composer-controls-location"
-                );
-                if (locationControl && anchorElement) {
-                  anchorElement.appendChild(locationControl);
+              if (!topicFirstPost) {
+                return false;
+              }
+              if (force) {
+                return true;
+              }
+              if (categoryId) {
+                const category = this.site.categories.findBy("id", categoryId);
+                if (category && category.custom_fields?.location_enabled) {
+                  return true;
                 }
               }
+              return false;
+            }
 
+            clearState() {
+              super.clearState(...arguments);
+              this.set("location", null);
+            }
+
+            @observes("draftKey")
+            _setupDefaultLocation() {
+              if (this.draftKey.startsWith(NEW_TOPIC_KEY)) {
+                const topicDefaultLocation =
+                  this.siteSettings.location_topic_default;
+                // NB: we can't use the siteSettings, nor currentUser values set in the initialiser here
+                // because in QUnit they will not be defined as the initialiser only runs once
+                // so this will break all tests, even if in runtime it may work.
+                // so solution is to use the values provided by the Composer model under 'this'.
+                if (
+                  topicDefaultLocation === "user" &&
+                  this.user.custom_fields.geo_location &&
+                  ((typeof this.user.custom_fields.geo_location === "string" &&
+                    this.user.custom_fields.geo_location.replaceAll(" ", "") !==
+                      "{}") ||
+                    (typeof this.user.custom_fields.geo_location === "object" &&
+                      Object.keys(this.user.custom_fields.geo_location)
+                        .length !== 0))
+                ) {
+                  this.set("location", {
+                    geo_location: this.user.custom_fields.geo_location,
+                  });
+                }
+              }
+            }
+          }
+      );
+
+      api.modifyClass(
+        "component:composer-body",
+        (Superclass) =>
+          class extends Superclass {
+            @observes("composer.location")
+            resizeWhenLocationAdded() {
               this._triggerComposerResized();
             }
-          };
 
-          scheduleOnce("afterRender", this, applyClasses);
-        }
-      });
+            @observes("composer.showLocationControls", "composer.composeState")
+            applyLocationInlineClass() {
+              const applyClasses = () => {
+                const showLocationControls = this.get(
+                  "composer.showLocationControls"
+                );
+                const containerElement = document.querySelector(
+                  ".composer-fields .title-and-category"
+                );
+
+                if (containerElement) {
+                  // Toggle the "show-location-controls" class based on `showLocationControls`
+                  if (showLocationControls) {
+                    containerElement.classList.add("show-location-controls");
+                  } else {
+                    containerElement.classList.remove("show-location-controls");
+                  }
+
+                  if (showLocationControls) {
+                    const anchorElement = this.site.mobileView
+                      ? containerElement.querySelector(".title-input")
+                      : containerElement;
+
+                    // Move ".composer-controls-location" element to `anchorElement`
+                    const locationControl = document.querySelector(
+                      ".composer-controls-location"
+                    );
+                    if (locationControl && anchorElement) {
+                      anchorElement.appendChild(locationControl);
+                    }
+                  }
+
+                  this._triggerComposerResized();
+                }
+              };
+
+              scheduleOnce("afterRender", this, applyClasses);
+            }
+          }
+      );
 
       const subtypeShowLocation = ["event", "question", "general"];
-      api.modifyClass("model:topic", Superclass => class extends Superclass {
-        // @computed("subtype", "category.custom_fields.location_enabled")
-        get showLocationControls() {
-          const subtype = this.get("subtype");
-          const categoryEnabled =
-            this.get("category.custom_fields.location_enabled");
-          return subtypeShowLocation.indexOf(subtype) > -1 || categoryEnabled;
-        }
-      });
+      api.modifyClass(
+        "model:topic",
+        (Superclass) =>
+          class extends Superclass {
+            // @computed("subtype", "category.custom_fields.location_enabled")
+            get showLocationControls() {
+              const subtype = this.get("subtype");
+              const categoryEnabled = this.get(
+                "category.custom_fields.location_enabled"
+              );
+              return (
+                subtypeShowLocation.indexOf(subtype) > -1 || categoryEnabled
+              );
+            }
+          }
+      );
 
       // necessary because topic-title plugin outlet only recieves model
-      api.modifyClass("controller:topic", Superclass => class extends Superclass {
-        @observes("editingTopic")
-        setEditingTopicOnModel() {
-          this.set("model.editingTopic", this.get("editingTopic"));
-        }
-      });
-
-      api.modifyClass("component:edit-category-settings", Superclass => class extends Superclass {
-        @discourseComputed("category.id", "category.custom_fields")
-        availableViews(categoryId, customFields) {
-          //category
-          let views = this._super(...arguments);
-
-          if (
-            category.get("custom_fields.location_enabled") &&
-            this.siteSettings.location_category_map_filter
-          ) {
-            views.push({ name: I18n.t("filters.map.label"), value: "map" });
+      api.modifyClass(
+        "controller:topic",
+        (Superclass) =>
+          class extends Superclass {
+            @observes("editingTopic")
+            setEditingTopicOnModel() {
+              this.set("model.editingTopic", this.get("editingTopic"));
+            }
           }
+      );
 
-          return views;
-        }
-      });
+      api.modifyClass(
+        "component:edit-category-settings",
+        (Superclass) =>
+          class extends Superclass {
+            @discourseComputed("category.id", "category.custom_fields")
+            availableViews() {
+              const category = this.get("category");
+              let views = super.availableViews(...arguments);
+
+              if (
+                category.get("custom_fields.location_enabled") &&
+                this.siteSettings.location_category_map_filter
+              ) {
+                views.push({ name: I18n.t("filters.map.label"), value: "map" });
+              }
+
+              return views;
+            }
+          }
+      );
 
       const mapRoutes = [`Map`, `MapCategory`, `MapCategoryNone`];
 
       mapRoutes.forEach(function (route) {
-        api.modifyClass(`route:discovery.${route}`, Superclass => class extends Superclass {
-          afterModel() {
-            this.templateName = "discovery/map";
+        api.modifyClass(
+          `route:discovery.${route}`,
+          (Superclass) =>
+            class extends Superclass {
+              afterModel() {
+                this.templateName = "discovery/map";
 
-            return this._super(...arguments);
-          }
-        });
+                return super.afterModel(...arguments);
+              }
+            }
+        );
       });
 
       const categoryRoutes = ["category", "categoryNone"];
 
       categoryRoutes.forEach(function (route) {
-        api.modifyClass(`route:discovery.${route}`, Superclass => class extends Superclass {
-          afterModel(model, transition) {
-            if (
-              this.filter(model.category) === "map" &&
-              this.siteSettings.location_category_map_filter
-            ) {
-              transition.abort();
-              return this.replaceWith(
-                `/c/${this.Category.slugFor(model.category)}/l/${this.filter(
-                  model.category
-                )}`
-              );
-            } else {
-              return this._super(...arguments);
+        api.modifyClass(
+          `route:discovery.${route}`,
+          (Superclass) =>
+            class extends Superclass {
+              afterModel(model, transition) {
+                if (
+                  this.filter(model.category) === "map" &&
+                  this.siteSettings.location_category_map_filter
+                ) {
+                  transition.abort();
+                  return this.replaceWith(
+                    `/c/${this.Category.slugFor(
+                      model.category
+                    )}/l/${this.filter(model.category)}`
+                  );
+                } else {
+                  return super.afterModel(...arguments);
+                }
+              }
             }
+        );
+      });
+      api.modifyClass("component:d-menu", (Superclass) => {
+        return class extends Superclass {
+          get options() {
+            // take the real options object from the instance
+            const base = this.menuInstance?.options ?? {};
+
+            // IMPORTANT: override inline from args at read-time
+            // so DFloatBody/DFloatPortal see it during the same render
+            if (
+              this.args?.inline !== undefined &&
+              base.inline !== this.args.inline
+            ) {
+              return { ...base, inline: this.args.inline };
+            }
+
+            return base;
           }
-        });
+        };
       });
     });
 
