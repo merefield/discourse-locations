647e23f2b9e27dbcf8b8426b1d7ab3f9afdf9d6d
merefield <merefield@gmail.com>
2026-01-31 23:42:29 +0000

    introduce specific map user serializer

--- app/serializers/locations/users_map_directory_item_serializer.rb
+++ app/serializers/locations/users_map_directory_item_serializer.rb
@@ -2,8 +2,20 @@
 
 module ::Locations
   class UsersMapDirectoryItemSerializer < ApplicationSerializer
-    class UserSerializer < UserNameSerializer
-      attributes :geo_location
+    class UserSerializer < ApplicationSerializer
+      attributes :id, :username, :name, :avatar_template, :geo_location
+
+      def name
+        object&.name
+      end
+
+      def include_name?
+        SiteSetting.enable_names?
+      end
+
+      def avatar_template
+        object&.avatar_template
+      end
 
       def geo_location
         geo = Locations.parse_geo_location(object.custom_fields["geo_location"])
