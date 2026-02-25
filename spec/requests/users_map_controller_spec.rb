39edd11bdf1acd3564538bf04dbdd58c7431d471
merefield <merefield@gmail.com>
2026-02-25 22:59:41 +0000

    fix user map request spec

--- spec/requests/users_map_controller_spec.rb
+++ spec/requests/users_map_controller_spec.rb
@@ -4,10 +4,13 @@ require "rails_helper"
 
 RSpec.describe ::Locations::UsersMapController do
   describe "GET /locations/users-map" do
+    let(:users_map_path) { "/locations/users-map.json" }
+    let(:legacy_users_map_path) { "/locations/users_map.json" }
+
     it "returns success for the canonical users-map route when plugin is enabled" do
       SiteSetting.location_enabled = true
 
-      get "/locations/users-map"
+      get users_map_path
 
       expect(response.status).to eq(200)
       expect(response.parsed_body["success"]).to eq("OK")
@@ -16,7 +19,7 @@ RSpec.describe ::Locations::UsersMapController do
     it "returns not found when plugin is disabled" do
       SiteSetting.location_enabled = false
 
-      get "/locations/users-map"
+      get users_map_path
 
       expect(response.status).to eq(404)
     end
@@ -24,7 +27,7 @@ RSpec.describe ::Locations::UsersMapController do
     it "does not route the legacy users_map path" do
       SiteSetting.location_enabled = true
 
-      get "/locations/users_map"
+      get legacy_users_map_path
 
       expect(response.status).to eq(404)
     end
