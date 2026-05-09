4dbd1c6d5f3441d58839125e8a0a457a732fdbbe
merefield <merefield@gmail.com>
2026-05-09 23:22:59 +0100

    CHORE: bump plugin minor version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 6.9.6
+# version: 7.2.0
 # authors: Robert Barrow, Angus McLeod
 # contact_emails: merefield@gmail.com
 # url: https://github.com/merefield/discourse-locations
