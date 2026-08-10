4a97a3daa83fd358b7bae29e2ea5ddbdc69fd9cf
merefield <merefield@gmail.com>
2026-08-10 22:40:21 +0100

    PERF: Limit users map serialization payload

--- spec/requests/directory_items_controller_locations_extensions_spec.rb
+++ spec/requests/directory_items_controller_locations_extensions_spec.rb
@@ -1,46 +1,82 @@
 # frozen_string_literal: true
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe DirectoryItemsController do
   fab!(:user)
 
-  context "when browsing the users map" do
-    before(:each) do
-      SiteSetting.location_enabled = true
-      sign_in(user)
+  describe "#index" do
+    context "when browsing the users map" do
+      before do
+        SiteSetting.location_enabled = true
+        sign_in(user)
+      end
+
+      it "returns the minimal map payload" do
+        SiteSetting.enable_names = true
+        map_user = Fabricate(:user, name: "Map User")
+        UserCustomField.create!(
+          user: map_user,
+          name: "geo_location",
+          value: {
+            lat: "51.5074",
+            lon: "-0.1278",
+            address: "London, United Kingdom"
+          }.to_json
+        )
+        Locations::UserLocationProcess.upsert(map_user.id)
+        DirectoryItem.refresh_period!(:daily, force: true)
+
+        SiteSetting.location_users_map = true
+        SiteSetting.enable_user_directory = true
+        get "/directory_items.json?period=location"
+
+        expect(response.status).to eq(200)
+        expect(response.parsed_body["directory_items"]).to contain_exactly(
+          {
+            "id" => map_user.id,
+            "user" => {
+              "id" => map_user.id,
+              "username" => map_user.username,
+              "name" => map_user.name,
+              "avatar_template" => map_user.avatar_template,
+              "geo_location" => {
+                "lat" => "51.5074",
+                "lon" => "-0.1278"
+              }
+            }
+          }
+        )
+      end
+
+      it "returns forbidden when the user directory is disabled" do
+        SiteSetting.location_users_map = true
+        SiteSetting.enable_user_directory = false
+        get "/directory_items.json?period=location"
+        expect(response.status).to eq(403)
+        expect(response.parsed_body["error_type"]).to eq("invalid_access")
+      end
+
+      it "returns forbidden when the users map is disabled" do
+        SiteSetting.location_users_map = false
+        SiteSetting.enable_user_directory = true
+        get "/directory_items.json?period=location"
+        expect(response.status).to eq(403)
+        expect(response.parsed_body["error_type"]).to eq("invalid_access")
+      end
     end
-    it "allows user to browse the users map" do
-      SiteSetting.location_users_map = true
-      SiteSetting.enable_user_directory = true
-      get "/directory_items.json?period=location"
-      expect(response.status).to eq(200)
-    end
-    it "doesn't allow user to browse the users map when user directory is disabled" do
-      SiteSetting.location_users_map = true
-      SiteSetting.enable_user_directory = false
-      get "/directory_items.json?period=location"
-      expect(response.status).to eq(403)
-      expect(response.parsed_body["error_type"]).to eq("invalid_access")
-    end
-    it "doesn't allow user to browse the users map when user map is disabled" do
-      SiteSetting.location_users_map = false
-      SiteSetting.enable_user_directory = true
-      get "/directory_items.json?period=location"
-      expect(response.status).to eq(403)
-      expect(response.parsed_body["error_type"]).to eq("invalid_access")
-    end
-  end
-  context "when the plugin is enabled but the user is not logged in" do
-    before do
-      SiteSetting.location_users_map = true
-      SiteSetting.enable_user_directory = true
-      SiteSetting.hide_user_profiles_from_public = true
-    end
-    it "doesn't allow user to browse the users map when the plugin is enabled but the user is not logged in" do
-      sign_out
-      get "/directory_items.json?period=location"
-      expect(response.status).to eq(403)
-      expect(response.parsed_body["error_type"]).to eq("invalid_access")
+
+    context "when profiles are hidden from anonymous users" do
+      before do
+        SiteSetting.location_users_map = true
+        SiteSetting.enable_user_directory = true
+        SiteSetting.hide_user_profiles_from_public = true
+      end
+
+      it "returns forbidden to an anonymous user" do
+        get "/directory_items.json?period=location"
+        expect(response.status).to eq(403)
+        expect(response.parsed_body["error_type"]).to eq("invalid_access")
+      end
     end
   end
 end
