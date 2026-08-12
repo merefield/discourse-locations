717a8f70a63c30293a9cf5a370af23e3927532ba
merefield <merefield@gmail.com>
2026-08-12 17:49:10 +0100

    DEV: Exclude unused topic proximity fix

--- spec/lib/locations/user_location_process_spec.rb
+++ spec/lib/locations/user_location_process_spec.rb
@@ -53,44 +53,4 @@ RSpec.describe Locations::UserLocationProcess do
       expect(Locations::UserLocation.exists?(user: user)).to eq(false)
     end
   end
-
-  describe ".search_users_from_topic_location" do
-    it "returns users near the topic" do
-      topic = Fabricate(:topic)
-      nearby_user = Fabricate(:user)
-      distant_user = Fabricate(:user)
-      timestamps = { created_at: Time.zone.now, updated_at: Time.zone.now }
-
-      Locations::TopicLocation.insert_all!(
-        [
-          {
-            topic_id: topic.id,
-            latitude: 51.5074,
-            longitude: -0.1278,
-            **timestamps
-          }
-        ]
-      )
-      Locations::UserLocation.insert_all!(
-        [
-          {
-            user_id: nearby_user.id,
-            latitude: 51.515,
-            longitude: -0.13,
-            **timestamps
-          },
-          {
-            user_id: distant_user.id,
-            latitude: 40.7128,
-            longitude: -74.006,
-            **timestamps
-          }
-        ]
-      )
-
-      expect(
-        described_class.search_users_from_topic_location(topic.id, 10)
-      ).to contain_exactly(nearby_user.id)
-    end
-  end
 end
