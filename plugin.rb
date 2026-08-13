559ea0a3cd2393522ef95f288a7a45cd19b66199
merefield <merefield@gmail.com>
2026-08-13 20:22:16 +0100

    DEV: Bump plugin version to 7.3.9

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 7.3.8
+# version: 7.3.9
 # authors: Robert Barrow, Angus McLeod
 # contact_emails: merefield@gmail.com
 # url: https://github.com/merefield/discourse-locations
